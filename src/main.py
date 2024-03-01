0c59997169ba16ba821a494cc21b289275ceef16
Hanyu Wu <whanyu1212@hotmail.com>
2024-03-01 23:50:53 +0800

    refactored main function

--- src/main.py
+++ src/main.py
@@ -3,11 +3,26 @@ import pandas as pd
 from src.data_processing import DataProcessor
 from src.model_pipeline import ModelPipeline
 from src.util.data_schema import input_data_schema
+from src.util.general_utility_functions import parse_cfg
 
-# Global variables
 
+def data_processing_flow(
+    data: pd.DataFrame, cutoff_date: str, categorical_column: str
+) -> pd.DataFrame:
+    """
+    Calls the data processing class to process the data.
 
-def data_processing_flow(data, cutoff_date, categorical_column):
+    Args:
+        data (pd.DataFrame): raw input data
+        cutoff_date (str): cut off date assigned to filter
+        out irrelevant data
+        categorical_column (str): column to be encoded
+
+    Returns:
+        pd.DataFrame: processed data
+    """
+
+    # Validate the input data schema and make sure they comply
     data = input_data_schema.validate(data)
     data_processor = DataProcessor(data, cutoff_date)
     processed_data = data_processor.process_flow(categorical_column)
@@ -15,25 +30,48 @@ def data_processing_flow(data, cutoff_date, categorical_column):
 
 
 def modelling_flow(
-    processed_data, test_splitting_date, validation_splitting_date, target_column
+    processed_data: pd.DataFrame,
+    test_splitting_date: str,
+    validation_splitting_date: str,
+    target_column: str,
 ):
+    """
+    Calls the model pipeline class (which calls the hyperparameter
+    tuning class as well) to train and evaluate the model.
+
+    Args:
+        processed_data (pd.DataFrame): data ready for splitting
+        and modelling
+        test_splitting_date (str): cut off date for splitting the
+        train/val and test data
+        validation_splitting_date (str): cut off date for splitting
+        the train and validation data
+        target_column (str): response column
+    """
     model_pipeline = ModelPipeline(
         processed_data, test_splitting_date, validation_splitting_date, target_column
     )
     model_pipeline.run_pipeline()
 
 
-def main():
-    data = pd.read_csv("./data/raw/input.csv")
-    cutoff_date = "2000-1-1"
-    categorical_column = "INDUSTRY2"
-    test_splitting_date = "2020-1-1"
-    val_splitting_date = "2015-1-1"
-    target_column = "Y"
+def main() -> None:
+    """Main function to chain everything together."""
+    config = parse_cfg("./config/catalog.yaml")
+    data = pd.read_csv(config["data"]["file_path"])
+    cutoff_date = config["dates"]["cutoff_date"]
+    categorical_column = config["columns"]["categorical_column"]
+    test_splitting_date = config["dates"]["test_splitting_date"]
+    val_splitting_date = config["dates"]["val_splitting_date"]
+    target_column = config["columns"]["target_column"]
     processed_data = data_processing_flow(data, cutoff_date, categorical_column)
     modelling_flow(
         processed_data, test_splitting_date, val_splitting_date, target_column
     )
+    # You should get roughly the following eval results:
+    # Model performance: {'accuracy': 0.9748966784019593,
+    # 'f1': 0.9658516530326045,
+    # 'pr_auc': 0.23979223005056124,
+    # 'roc_auc': 0.8875964770170739}
 
 
 if __name__ == "__main__":
